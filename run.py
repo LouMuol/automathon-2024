524e57da2f18022e119d98988587b9a030e705a0
Moriceau Louis <louis.moriceau@student-cs.fr>
2024-05-01 07:50:23 +0200

    CNN

--- run.py
+++ run.py
@@ -229,11 +229,25 @@ experimental_dataset = VideoDataset(
 class DeepfakeDetector(nn.Module):
     def __init__(self, nb_frames=10):
         super().__init__()
-        self.dense = nn.Linear(nb_frames*3*256*256, 1)
+        self.dense = nn.Linear(nb_frames*3*256*256*64, 1)
+        self.layer1 = nn.Conv2d(3, 16, 3, 1)
+        self.layer2 = nn.Conv2d(16, 32, 3, 1)
+        self.layer3 = nn.Conv2d(32, 64, 3, 1)
+        self.ReLU = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
         self.flat = nn.Flatten()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+        x = self.layer1(x)
+        x = self.ReLU(x)
+        x = self.pool(x)
+        x = self.layer2(x)
+        x = self.ReLU(x)
+        x = self.pool(x)
+        x = self.layer3(x)
+        x = self.ReLU(x)
+        x = self.pool(x)
         y = self.flat(x)
         y = self.dense(y)
         y = self.sigmoid(y)
