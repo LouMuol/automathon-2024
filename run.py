b856004d6f5958aa2d4f86408246fa3d76ceeae2
Moriceau Louis <louis.moriceau@student-cs.fr>
2024-05-01 10:00:49 +0200

    wandbFix

--- run.py
+++ run.py
@@ -239,9 +239,9 @@ experimental_dataset = VideoDataset(
 class DeepfakeDetector(nn.Module):
     def __init__(self, nb_frames=10):
         super().__init__()
-        self.dense = nn.Linear(1*62*62*64, 1)
-        self.layer1 = nn.Conv3d(3, 32, 3)
-        self.layer2 = nn.Conv3d(32, 64, 3)
+        self.dense = nn.Linear(1*62*62*32, 1)
+        self.layer1 = nn.Conv3d(3, 16, 3)
+        self.layer2 = nn.Conv3d(16, 32, 3)
         self.ReLU = nn.ReLU()
         self.pool = nn.MaxPool3d(2)
         self.flat = nn.Flatten()
