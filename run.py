d40bf4ca7f846493f0cdbd143f15f5af58d5dc5b
Moriceau Louis <louis.moriceau@student-cs.fr>
2024-05-01 16:19:33 +0200

    mkdir

--- run.py
+++ run.py
@@ -88,14 +88,14 @@ def resize_data(data, new_height, new_width, x=0, y=0, height=None, width=None):
 
 # SETUP DATASET
 
-dataset_dir = "/raid/datasets/hackathon2024"
+dataset_dir = "../anon/automathon-2024/"
 root_dir = os.path.expanduser("~/automathon-2024")
 nb_frames = 10
 
 # MAKE RESIZED DATASET
 resized_dir = os.path.join(dataset_dir, "resized_dataset")
-"""
-create_small_dataset = False
+
+create_small_dataset = True
 errors = []
 if not os.path.exists(resized_dir) or create_small_dataset:
     os.mkdir(resized_dir)
@@ -112,15 +112,14 @@ if not os.path.exists(resized_dir) or create_small_dataset:
     def resize(in_video_path, out_video_path, nb_frames=10):
         video = extract_frames(in_video_path, nb_frames=nb_frames)
         t1 = time.time()
-        #video, audio, info = io.read_video(in_video_path, pts_unit='sec', start_pts=0, end_pts=10, output_format='TCHW')
+        # video, audio, info = io.read_video(in_video_path, pts_unit='sec', start_pts=0, end_pts=10, output_format='TCHW')
         video = smart_resize(video, 256)
         t2 = time.time()
         torch.save(video, out_video_path)
         t3 = time.time()
         print(f"resize: {t2-t1}\nsave: {t3-t2}")
-        #video = video.permute(0,2,3,1)
-        #io.write_video(video_path, video, 15, video_codec='h264')
-
+        # video = video.permute(0,2,3,1)
+        # io.write_video(video_path, video, 15, video_codec='h264')
 
     for f in tqdm(train_files):
         in_video_path = os.path.join(dataset_dir, "train_dataset", f)
@@ -156,7 +155,7 @@ if not os.path.exists(resized_dir) or create_small_dataset:
     os.system(f"cp {os.path.join(dataset_dir, 'experimental_dataset', 'metadata.json')} {os.path.join(resized_dir, 'experimental_dataset', 'metadata.json')}")
     if errors:
         print(errors)
-"""
+
 use_small_dataset = True
 if use_small_dataset:
     dataset_dir = resized_dir
