6d7fa45e51f0d366e3a2fcdffdf78c862740730a
Moriceau Louis <louis.moriceau@student-cs.fr>
2024-05-01 14:40:57 +0200

    C:/Program Files/Git/10

--- run.py
+++ run.py
@@ -250,7 +250,7 @@ class DeepfakeDetector(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        y = x[:, 0]
+        y = x[:, :, 0]
         y = self.encoder(y)
         y = self.flat(y)
         y = self.dense(y)
