d51bd88e885cc3a800149ebc73db68215e5e8c3c
Moriceau Louis <louis.moriceau@student-cs.fr>
2024-05-01 09:51:01 +0200

    wandbFix

--- run.py
+++ run.py
@@ -270,20 +270,11 @@ run = wandb.init(
     config={
         "learning_rate": 0.02,
         "architecture": "CNN",
-        "dataset": "CIFAR-100",
-        "epochs": 10,
+        "dataset": "basique",
+        "epochs": 5,
+        "batch_size": 32
     }
 )
-
-# simulate training
-epochs = 10
-offset = random.random() / 5
-for epoch in range(2, epochs):
-    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
-    loss = 2 ** -epoch + random.random() / epoch + offset
-
-    # log metrics to wandb
-    wandb.log({"acc": acc, "loss": loss})
 
 # ENTRAINEMENT
 
